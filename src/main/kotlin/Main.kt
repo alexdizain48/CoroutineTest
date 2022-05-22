6ac1ac5560c0f1a8bc618c87253dfb784ecaa713
Alexandr <alart48@yandex.ru>
2022-05-22 10:46:28 +0300

    create Double dbl 1,2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -91,6 +91,10 @@ fun main() {
     val bl1: Int = 0
     val bl2: Int = 0
 
+    val dbl: Double = 0.3
+    val dbl1: Double = 0.4
+    val dbl2: Double = 0.5
+
     val paiyment: PaymentOption = PaymentOption.values().random()
     println(paiyment.commission)
 
